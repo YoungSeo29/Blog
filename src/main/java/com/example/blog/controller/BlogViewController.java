e55f247c1d38e6b06ba3e1c2c7014af4fd0f6bed
youngsuh <yseokim0212@gmail.com>
2025-04-21 10:47:03 +0900

    feat: 글 상세 페이지, 페이지 연결

--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.blog.controller;
 
+import com.example.blog.domain.Article;
 import com.example.blog.dto.ArticleListViewResponseDto;
+import com.example.blog.dto.ArticleViewResponseDto;
 import com.example.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,5 +29,14 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable("id") Long id, Model model) {
+
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponseDto(article));
+
+        return "article";
     }
 }
