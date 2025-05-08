43f2e9da0a8054d75eef6c36b5fd67ff76138f60
youngsuh <yseokim0212@gmail.com>
2025-05-08 12:05:40 +0900

    chore: 환경변수 파일 추가

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -3,12 +3,23 @@ package com.example.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
+
+        // .env 파일 로딩
+        Dotenv dotenv = Dotenv.load();
+
+        // 환경변수로 등록
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(BlogApplication.class, args);
     }
 
