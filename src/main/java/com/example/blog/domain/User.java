57da3a7f4cbfbc6a148cdbd643bc8a655fb64a7a
youngsuh <yseokim0212@gmail.com>
2025-05-11 01:02:11 +0900

    feat: 사용자 정보 불러오는 메서드

--- src/main/java/com/example/blog/domain/User.java
+++ src/main/java/com/example/blog/domain/User.java
@@ -26,13 +26,17 @@ public class User implements UserDetails {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Column(name = "password")
     private String password;
 
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
     this.email = email;
     this.password = password;
+    this.nickname = nickname;
     }
 
     @Override
@@ -68,5 +72,11 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
