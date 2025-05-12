3fc7f86b742239e39db80c7c77b169d8f5dc0654
youngsuh <yseokim0212@gmail.com>
2025-05-12 10:01:45 +0900

    comment : WebOAuthSecurityConfig 주석 작성

--- src/main/java/com/example/blog/config/WebOAuthSecurityConfig.java
+++ src/main/java/com/example/blog/config/WebOAuthSecurityConfig.java
@@ -19,7 +19,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+/*
+기본 WebSecurityConfig는 세션 기반 인증 방식에 필요함
+WebOAuthSecurityConfig는 JWT 기반 인증 방식.
 
+** 세션 기반 인증 :
+    인증 성공하면, 서버에서 세션 객체 생성 -> 클라에게 JSESSIONID 쿠키를 줌
+** JWT 기반 인증 :
+    로그인 성공 시, 서버가 JWT 토큰 만들어서 클라에게 줌 -> 클라는 매 요청마다 토큰을 Authorization 헤더에 담아서 보냄.
+ */
 @RequiredArgsConstructor
 @Configuration
 public class WebOAuthSecurityConfig {
@@ -29,34 +37,42 @@ public class WebOAuthSecurityConfig {
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
 
+    // 스프링 시큐리티 기능 비활성화
+    // = 인증, 인가 필터 모두 스킵 (정적 리소스는 비활성화 해도 됨)
     @Bean
     public WebSecurityCustomizer configure() {
 
-        // 스프링 시큐리티 기능 비활성화
-        // = 인증, 인가 필터 모두 스킵 (정적 리소스는 비활성화 해도 됨)
         return (web) -> web.ignoring()
                 .requestMatchers("/img/**", "/css/**", "/jss/**");
     }
+
+
+    // 시큐리티 전반적인 필터 체인 구성
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // 토큰 방식으로 인증을 하기 때문에, 기존에 사용하던 폼 로그인, 세션 비활성화
         return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)  // csrf 보호 비활성화(JWT 기반이라 필요없음)
+                .httpBasic(AbstractHttpConfigurer::disable)  // 브라우저 기본 인증창 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)  // 폼 로그인 비활성화(JWT 쓰니까)
+                .logout(AbstractHttpConfigurer::disable)  // 로그아웃 처리 비활성화
+                // 세션을 아예 사용하지 않음
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // JWT 필터를 시큐리티 필터 체인 앞에 등록
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                // 인증, 인가 설정
                 .authorizeRequests(auth -> auth
                         .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated().anyRequest().permitAll())
+                // Oauth2 로그인 설정
                 .oauth2Login(oauth2 -> oauth2.loginPage("/login")
                         .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint.authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
                         .successHandler(oAuth2SuccessHandler())
                 )
+                // 인증 실패 시 에러 처리 방식 결정
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .defaultAuthenticationEntryPointFor(
                                 new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
@@ -65,6 +81,8 @@ public class WebOAuthSecurityConfig {
                 .build();
     }
 
+
+    // Oauth2 로그인 성공 이후 할 작업을 정의하는 핸들러 등록
     @Bean
     public OAuth2SuccessHandler oAuth2SuccessHandler() {
         return new Oauth2SuccessHandler(
@@ -75,11 +93,15 @@ public class WebOAuthSecurityConfig {
         );
     }
 
+
+    // 요청 헤더이 있는 JWT 토큰을 파싱 -> 인증 객체 생성 -> SpringContext 에 저장하는 필터
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter(tokenProvider);
     }
 
+
+    // 로그인 과정 중, 인가 요청 정보를 쿠키로 저장, 복원하는 커스텀 레포지토리
     @Bean
     public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
         return new OAuth2AuthorizationRequestBasedOnCookieRepository();
