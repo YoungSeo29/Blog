a55404eb42221024994d04e2426b24ce99358347
youngsuh <yseokim0212@gmail.com>
2025-05-16 18:00:29 +0900

    feat: OAuth2AuthorizationRequest 객체 저장, 복원

--- src/main/java/com/example/blog/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/com/example/blog/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -8,30 +8,42 @@ import org.springframework.security.oauth2.client.web.AuthorizationRequestReposi
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.web.util.WebUtils;
 
+// OAuth2에 필요한 정보를 세션이 아니라 쿠키에 저장해서 쓸 수 있도록,
+// 인증과 관련된 상태를 저장할 저장소.
 public class OAuth2AuthorizationRequestBasedOnCookieRepository
         implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
     private final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    private final static int COOKIE_EXPIRE_SECONDS = 18000;
 
-    @Override
-    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
-        return this.loadAuthorizationRequest(request);
-    }
 
+    // 인가 요청 보내기 전에, OAuth2AuthorizationRequest 객체를 쿠키에 저장함.
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
         if(authorizationRequest == null) {
             removeAuthorizationRequest(request, response);
             return;
         }
+
+        CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME, CookieUtil.serialize(authorizationRequest), COOKIE_EXPIRE_SECONDS);
     }
 
+    // 카카오한테 인가코드 + state 받고 나서, 쿠키에서 원래 저장해둔 정보 꺼냄.
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
         Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
 
         return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class);
     }
+
+
+    // 검증 끝나고 쿠키 지움
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
+    }
+
+
 
     public void removeAuthorizationRequestCookie(HttpServletRequest request, HttpServletResponse response) {
         CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
