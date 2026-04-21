b1dd7641c4dae8aa087758de0d69be4554fc4de4
youngsuh <yseokim0212@gmail.com>
2026-04-21 22:27:24 +0900

    update: DB_URL 환경변수 파일 수정

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -19,6 +19,7 @@ public class BlogApplication {
         System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
 
         SpringApplication.run(BlogApplication.class, args);
     }
